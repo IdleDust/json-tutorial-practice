a043f2149f957af85168f94bf562516fd6763cc0
JuanChenMacbookAir <JuanChen@JuanChendeMacBook-Air.local>
2017-03-13 18:44:35 -0700

    test

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -84,5 +84,6 @@ static void test_parse() {
 int main() {
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+    printf("%d", main_ret);
     return main_ret;
 }
